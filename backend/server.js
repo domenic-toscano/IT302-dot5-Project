0599c97c1a14de6beaa54461e9b421fbed8a3462
Domenic Toscano <124611852+domenic-toscano@users.noreply.github.com>
2025-10-05 23:43:11 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+// Domenic Toscano
+// 10/5/25
+// IT302-451
+// Project Phase 2
+// dot5@njit.edu
+
 const express = require("express");
 const cors = require("cors");
 
@@ -7,10 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Mount all routes under your UCID root
 app.use("/api/v1/dot5", bikesRouter);
 
-// Optional quick health check
 app.get("/", (_req, res) => res.send("Backend is running."));
 
 module.exports = app;
